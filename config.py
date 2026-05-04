9a88d0dd069fbaacede7fc786bc62cbae0ff8511
Cristiano Singulani <crisingulani@gmail.com>
2026-05-04 17:23:06 -0300

    Update Slurm configuration and sbatch template for improved resource allocation

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ DASK_EXECUTOR = os.getenv("DASK_EXECUTOR", "local")
 class Slurm(BaseModel):
 
     class Instance(BaseModel):
-        cores: int = 25
-        processes: int = 1
+        cores: int = 20
+        processes: int = 4
         memory: str = "50GB"
-        queue: str = "cpu"
-        account: str = "hpc-bpglsst"
+        queue: str = "cpu_pipelines"
+        account: str = "hpc-pipelines"
         job_extra_directives: list[str] = ["--propagate", "--time=2:00:00"]
 
     class Scale(BaseModel):
