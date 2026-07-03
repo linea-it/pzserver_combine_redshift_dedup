683a5b6b4665213dcb7d96c5c04631800de1779b
Luigi Lucas de Carvalho Silva <luigi.lcsilva@gmail.com>
2026-07-02 22:49:18 -0300

    Reducing radius default to 0.5 arcsec in crd-run.py

--- scripts/crd-run.py
+++ scripts/crd-run.py
@@ -712,7 +712,7 @@ def main(
         log_init.error("Failed to parse flags_translation_file: %s", e, exc_info=True)
         raise
     crossmatch_radius_arcsec = float(
-        translation_config.get("crossmatch_radius_arcsec", 0.75)
+        translation_config.get("crossmatch_radius_arcsec", 0.5)
     )
     margin_threshold_arcsec = float(
         translation_config.get("margin_threshold_arcsec", 5.0)
