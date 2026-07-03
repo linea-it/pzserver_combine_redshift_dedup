88477abb4ec4ec3d48984a7a5162669a837153d9
Luigi Lucas de Carvalho Silva <luigi.lcsilva@gmail.com>
2026-07-02 22:44:22 -0300

    Incresing n_neighbors to 40

--- packages/crossmatch_cross.py
+++ packages/crossmatch_cross.py
@@ -792,7 +792,7 @@ def crossmatch_tiebreak(
 
     # Parameters with defaults
     radius = float((translation_config or {}).get("crossmatch_radius_arcsec", 0.75))
-    k = int((translation_config or {}).get("crossmatch_n_neighbors", 20))
+    k = int((translation_config or {}).get("crossmatch_n_neighbors", 40))
     saturation_enabled = bool(
         (translation_config or {}).get("crossmatch_saturation_enabled", False)
     )
